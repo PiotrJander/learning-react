0304a79ef4ce765ee79021456cb9fef178431e5f
PiotrJander <pjander2@gmail.com>
2016-06-25 20:21:47 +0100

    Finally, state!

--- components/comments.js
+++ components/comments.js
@@ -1,13 +1,46 @@
 import React from 'react'
 
 
+let comments = [
+  {
+    author: 'Piotr',
+    contents: 'This is awesome'
+  }, {
+    author: 'Filip',
+    contents: 'This is bad'
+  }, {
+    author: 'Justin',
+    contents: 'This is shit'
+  }
+];
+
 export class CommentList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.updateNumberOfComments = this.updateNumberOfComments.bind(this);
+    this.state = {
+      numberOfComments: 2
+    }
+  }
+
+  updateNumberOfComments(event) {
+    console.log('value', event.target.value);
+    this.setState({
+      numberOfComments: event.target.value
+    })
+  }
+
   render() {
+    let noc = this.state.numberOfComments;
     return (
       <div>
-        <Comment contents={'foo'} />
-        <Comment contents={'bar'} />
-        <Comment contents={'baz'} />
+        <input type="number" value={noc} onChange={this.updateNumberOfComments} />
+        {comments.slice(0, noc).map(comment => (
+          <Comment
+            contents={comment.contents}
+            author={comment.author}
+          />
+        ))}
       </div>
     )
   }
@@ -17,11 +50,14 @@ export class Comment extends React.Component {
   render() {
     // let p = this.props;
     return (
-      <p>{this.props.contents}</p>
+      <div>
+        <p>{this.props.author}: {this.props.contents}</p>
+      </div>
     )
   }
 }
 
 Comment.propTypes = {
-  contents: React.PropTypes.string
+  contents: React.PropTypes.string,
+  author: React.PropTypes.string
 };
